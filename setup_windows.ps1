8b1d1ebd89f42caedf715335189f107feac74df8
Taylor Gordon <taylorgordon20@gmail.com>
2018-08-27 21:33:23 -0400

    Add windows set up powershell script

---
+++ setup_windows.ps1
@@ -0,0 +1,10 @@
+choco install colortool
+choco install vscode
+choco install git
+choco install python3
+choco install pip
+choco install vcbuildtools
+choco install cmake --installargs '"ADD_CMAKE_TO_PATH=System"'
+choco install bazel
+choco install llvm
+pip install -r requirements.txt
