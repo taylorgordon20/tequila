8eab4ff00c0a7c45ea32ccc75993464426459c0a
Taylor Gordon <taylorgordon20@gmail.com>
2018-09-21 20:18:35 -0400

    Basic JS embed

---
+++ src/common/js.hpp
@@ -0,0 +1,310 @@
+#pragma once
+
+#include <ChakraCore.h>
+
+#include <array>
+#include <string>
+#include <unordered_map>
+
+#include "src/common/errors.hpp"
+#include "src/common/strings.hpp"
+
+namespace tequila {
+
+#define JS_ENFORCE(js_call)                                                \
+  do {                                                                     \
+    auto status = (js_call);                                               \
+    if (status == JsErrorScriptException) {                                \
+      throwJsError();                                                      \
+    } else if (status == JsErrorScriptCompile) {                           \
+      throwJsError();                                                      \
+    } else if (status != JsNoError) {                                      \
+      throwError("JsError: '%1%' at %2%:%3%", status, __FILE__, __LINE__); \
+    }                                                                      \
+  } while (0)
+
+class JsModules {
+ public:
+  JsModules() {
+    JS_ENFORCE(JsCreateRuntime(JsRuntimeAttributeNone, nullptr, &runtime_));
+    JS_ENFORCE(JsCreateContext(runtime_, &context_));
+  }
+
+  ~JsModules() noexcept {
+    JsDisposeRuntime(runtime_);
+  }
+
+  int64_t load(const std::string& code) {
+    ContextGuard ctx_guard(context_);
+
+    static int64_t script_id_counter = 0;
+    auto script_id = script_id_counter++;
+    auto script_name = format("%1%.js", script_id);
+
+    // Load the code into a JS value.
+    JsValueRef script;
+    JS_ENFORCE(JsCreateExternalArrayBuffer(
+        (void*)code.c_str(), code.size(), nullptr, nullptr, &script));
+
+    // Run the script and store its result into a module reference.
+    JsSourceContext src_ctx = 0;
+    JsValueRef src_url = value(script_name);
+    JsValueRef module;
+    JS_ENFORCE(
+        JsRun(script, src_ctx, src_url, JsParseScriptAttributeNone, &module));
+
+    JS_ENFORCE(JsAddRef(module, nullptr));
+    modules_[script_id] = module;
+    return script_id;
+  }
+
+  void drop(int64_t script_id) {
+    ContextGuard ctx_guard(context_);
+    auto module = modules_.at(script_id);
+    modules_.erase(script_id);
+    JsRelease(module, nullptr);
+  }
+
+  template <typename Return, typename... Args>
+  Return call(int64_t script_id, const std::string& fn, Args&&... args) {
+    ContextGuard ctx_guard(context_);
+    auto& module = modules_.at(script_id);
+
+    // Get a reference to the JS function.
+    JsValueRef fn_ref = property(module, fn);
+
+    // Prepare the function arguments.
+    std::array<JsValueRef, 1 + sizeof...(Args)> fn_args;
+    fn_args.at(0) = undefined();
+    {
+      size_t i = 1;
+      (void(fn_args[i++] = value(std::forward<Args>(args))), ...);
+    }
+
+    // Invoke the function and return its return value.
+    JsValueRef fn_ret;
+    JS_ENFORCE(JsCallFunction(fn_ref, fn_args.data(), fn_args.size(), &fn_ret));
+    return cast<Return>(fn_ret);
+  }
+
+  template <typename Arg>
+  void setGlobal(const std::string& name, Arg&& arg) {
+    ContextGuard ctx_guard(context_);
+    setProperty(global(), name, value(std::forward<Arg>(arg)));
+  }
+
+  template <typename Return>
+  Return getGlobal(const std::string& name) {
+    ContextGuard ctx_guard(context_);
+    return cast<Return>(property(global(), name));
+  }
+
+ protected:
+  class ContextGuard {
+   public:
+    ContextGuard(JsContextRef context) {
+      ENFORCE(JsNoError == JsSetCurrentContext(context));
+    }
+    ~ContextGuard() noexcept {
+      JsSetCurrentContext(JS_INVALID_REFERENCE);
+    }
+  };
+
+  // Simple value creation functions.
+  JsValueRef undefined();
+  JsValueRef global();
+  JsValueRef value(double value);
+  JsValueRef value(int value);
+  JsValueRef value(bool value);
+  JsValueRef value(const char* value);
+  JsValueRef value(const std::string& value);
+
+  // Object value creation functions.
+  template <typename ValueType>
+  JsValueRef value(const std::vector<ValueType>& value);
+
+  template <typename ValueType>
+  JsValueRef value(const std::unordered_map<std::string, ValueType>& value);
+
+  // Object property functions.
+  JsPropertyIdRef propertyId(const std::string& prop);
+  JsValueRef property(JsValueRef obj, const std::string& prop);
+  void setProperty(JsValueRef obj, const std::string& prop, JsValueRef value);
+
+  // Array index functions.
+  JsValueRef index(JsValueRef obj, int index);
+  void setIndex(JsValueRef obj, int index, JsValueRef value);
+
+  // Value dereferencing functions.
+  template <typename ValueType>
+  void castTo(JsValueRef value, ValueType& out);
+
+  template <typename ValueType>
+  void castTo(JsValueRef value, std::vector<ValueType>& out);
+
+  template <typename ValueType>
+  void castTo(
+      JsValueRef value, std::unordered_map<std::string, ValueType>& out);
+
+  template <typename ReturnType>
+  ReturnType cast(JsValueRef value) {
+    ReturnType ret;
+    castTo(value, ret);
+    return ret;
+  }
+
+  // Error handling.
+  void throwJsError() {
+    JsValueRef exception;
+    JS_ENFORCE(JsGetAndClearException(&exception));
+    throwError(
+        "JsError: '%1%' Message: '%2%'",
+        cast<std::string>(property(exception, "name")),
+        cast<std::string>(property(exception, "message")));
+  }
+
+ private:
+  JsRuntimeHandle runtime_;
+  JsContextRef context_;
+  std::unordered_map<int64_t, JsValueRef> modules_;
+};
+
+JsValueRef JsModules::undefined() {
+  JsValueRef ret;
+  JS_ENFORCE(JsGetUndefinedValue(&ret));
+  return ret;
+}
+
+JsValueRef JsModules::global() {
+  JsValueRef ret;
+  JS_ENFORCE(JsGetGlobalObject(&ret));
+  return ret;
+}
+
+JsValueRef JsModules::value(double value) {
+  JsValueRef ret;
+  JS_ENFORCE(JsDoubleToNumber(value, &ret));
+  return ret;
+}
+
+JsValueRef JsModules::value(int value) {
+  JsValueRef ret;
+  JS_ENFORCE(JsIntToNumber(value, &ret));
+  return ret;
+}
+
+JsValueRef JsModules::value(bool value) {
+  JsValueRef ret;
+  JS_ENFORCE(JsBoolToBoolean(value, &ret));
+  return ret;
+}
+
+JsValueRef JsModules::value(const char* value) {
+  return this->value(std::string(value));
+}
+
+JsValueRef JsModules::value(const std::string& value) {
+  JsValueRef ret;
+  JS_ENFORCE(JsCreateString(value.c_str(), value.size(), &ret));
+  return ret;
+}
+
+template <typename ValueType>
+JsValueRef JsModules::value(const std::vector<ValueType>& value) {
+  JsValueRef ret;
+  JS_ENFORCE(JsCreateArray(value.size(), &ret));
+  for (int i = 0; i < value.size(); i += 1) {
+    JS_ENFORCE(
+        JsSetIndexedProperty(ret, this->value(i), this->value(value.at(i))));
+  }
+  return ret;
+}
+
+template <typename ValueType>
+JsValueRef JsModules::value(
+    const std::unordered_map<std::string, ValueType>& value) {
+  JsValueRef ret;
+  JS_ENFORCE(JsCreateObject(&ret));
+  for (const auto& pair : value) {
+    setProperty(ret, pair.first, this->value(pair.second));
+  }
+  return ret;
+}
+
+JsPropertyIdRef JsModules::propertyId(const std::string& property) {
+  JsPropertyIdRef ret;
+  JS_ENFORCE(JsCreatePropertyId(property.c_str(), property.size(), &ret));
+  return ret;
+}
+
+JsValueRef JsModules::property(JsValueRef obj, const std::string& prop) {
+  JsValueRef ret;
+  JS_ENFORCE(JsGetProperty(obj, propertyId(prop), &ret));
+  return ret;
+}
+
+void JsModules::setProperty(
+    JsValueRef object, const std::string& prop, JsValueRef val) {
+  JS_ENFORCE(JsSetProperty(object, propertyId(prop), val, true));
+}
+
+JsValueRef JsModules::index(JsValueRef object, int index) {
+  JsValueRef ret;
+  JS_ENFORCE(JsGetIndexedProperty(object, value(index), &ret));
+  return ret;
+}
+
+void JsModules::setIndex(JsValueRef object, int index, JsValueRef val) {
+  JS_ENFORCE(JsSetIndexedProperty(object, value(index), val));
+}
+
+template <typename... Args>
+void call(int64_t script_id, const std::string& fn, Args&&... args) {
+  auto& module = modules_.at(script_id);
+  JS_ENFORCE(JsCallFunction(property(module, fn), nullptr, 0, nullptr));
+}
+
+template <>
+void JsModules::castTo(JsValueRef value, std::string& out) {
+  size_t length;
+  JS_ENFORCE(JsCopyString(value, nullptr, 0, &length));
+
+  out.resize(length);
+  JS_ENFORCE(JsCopyString(value, out.data(), length, &length));
+}
+
+template <>
+void JsModules::castTo(JsValueRef value, double& out) {
+  JS_ENFORCE(JsNumberToDouble(value, &out));
+}
+
+template <>
+void JsModules::castTo(JsValueRef value, int& out) {
+  JS_ENFORCE(JsNumberToInt(value, &out));
+}
+
+template <>
+void JsModules::castTo(JsValueRef value, bool& out) {
+  JS_ENFORCE(JsBooleanToBool(value, &out));
+}
+
+template <typename ValueType>
+void JsModules::castTo(JsValueRef value, std::vector<ValueType>& out) {
+  int out_size = cast<int>(property(value, "length"));
+  out.resize(out_size);
+  for (int i = 0; i < out_size; i += 1) {
+    castTo(index(value, i), out[i]);
+  }
+}
+
+template <typename ValueType>
+void JsModules::castTo(
+    JsValueRef value, std::unordered_map<std::string, ValueType>& out) {
+  JsValueRef property_names;
+  JsGetOwnPropertyNames(value, &property_names);
+  for (auto& prop : cast<std::vector<std::string>>(property_names)) {
+    out[prop] = cast<ValueType>(property(value, prop));
+  }
+}
+
+}  // namespace tequila
---
+++ src/test/js_test.cpp
@@ -0,0 +1,173 @@
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+
+#include "src/common/js.hpp"
+
+namespace tequila {
+
+constexpr auto kScript1 = R"(
+var module = {};
+
+module.foo = function() {
+  return "foo_string";
+};
+
+module.bar = function(a_number) {
+  return "bar_string:" + (2 * a_number);
+};
+
+module.two_args = function(a_number, a_string) {
+  return "two_args:" + (3 * a_number) + a_string;
+};
+
+module.four_args = function(a_number, a_bool, a_string, a_int) {
+  return "four_args:" + a_number + ":" + a_bool + ":" + a_string + ":" + a_int;
+};
+
+module.array_args = function(number_array) {
+  var ret = "";
+  for (var i = 0; i < number_array.length; i += 1) {
+    ret += number_array[i];
+  }
+  return ret;
+};
+
+module;
+)";
+
+constexpr auto kScript2 = R"(
+var module = {};
+
+module.add = function(num1, num2) {
+  return num1 + num2;
+}
+
+module.mul = function(num1, num2) {
+  return num1 * num2;
+}
+ 
+module.arrcat = function(num1, num2, opt_num3) {
+  if (typeof opt_num3 !== 'undefined') {
+    return [num1, num2, opt_num3];
+  } else {
+    return [num1, num2];
+  }
+}
+
+module.strcat = function(num1, num2) {
+  return ("" + num1) + num2;
+}
+
+module;
+)";
+
+constexpr auto kScript3 = R"(
+var module = {};
+
+module.get_a_number = function() {
+  return a_number;
+}
+
+module.get_a_bool = function() {
+  return a_bool;
+}
+
+module.get_an_int = function() {
+  return an_int;
+}
+
+module.get_a_string = function() {
+  return a_string;
+}
+
+module.get_an_array = function() {
+  return an_array;
+}
+
+module.get_a_map = function() {
+  return a_map;
+}
+
+module;
+)";
+
+TEST_CASE("Test argument types", "[js]") {
+  JsModules js;
+
+  auto script_1 = js.load(kScript1);
+  REQUIRE("foo_string" == js.call<std::string>(script_1, "foo"));
+  REQUIRE("bar_string:6" == js.call<std::string>(script_1, "bar", 3.0));
+  REQUIRE(
+      "two_args:9dogs" ==
+      js.call<std::string>(script_1, "two_args", 3.0, "dogs"));
+  REQUIRE(
+      "four_args:-1:true:lies:42" ==
+      js.call<std::string>(script_1, "four_args", -1.0, true, "lies", 42));
+  REQUIRE(
+      "123456" ==
+      js.call<std::string>(
+          script_1, "array_args", std::vector<double>{1, 2, 3, 4, 5, 6}));
+}
+
+TEST_CASE("Test return types", "[js]") {
+  using namespace Catch::Matchers;
+
+  JsModules js;
+
+  auto script_2 = js.load(kScript2);
+  REQUIRE(4 == js.call<int>(script_2, "add", 3, 1));
+  REQUIRE(3.0 == js.call<double>(script_2, "mul", 3, 1));
+  REQUIRE("31" == js.call<std::string>(script_2, "strcat", 3, 1));
+  REQUIRE_THAT(
+      js.call<std::vector<double>>(script_2, "arrcat", 3, 1),
+      Equals<double>({3.0, 1.0}));
+  REQUIRE_THAT(
+      js.call<std::vector<int>>(script_2, "arrcat", 4, 5, 6),
+      Equals<int>({4, 5, 6}));
+}
+
+TEST_CASE("Test globals", "[js]") {
+  using namespace Catch::Matchers;
+
+  JsModules js;
+  js.setGlobal("a_number", 3.2);
+  js.setGlobal("a_bool", true);
+  js.setGlobal("an_int", 7);
+  js.setGlobal("a_string", "foobar");
+  js.setGlobal("an_array", std::vector<std::string>{"hello", "joe"});
+  js.setGlobal(
+      "a_map",
+      std::unordered_map<std::string, int>{
+          {"hello", 1},
+          {"bloe", 2},
+      });
+  REQUIRE(3.2 == js.getGlobal<double>("a_number"));
+  REQUIRE(true == js.getGlobal<bool>("a_bool"));
+  REQUIRE(7 == js.getGlobal<int>("an_int"));
+  REQUIRE("foobar" == js.getGlobal<std::string>("a_string"));
+  REQUIRE_THAT(
+      js.getGlobal<std::vector<std::string>>("an_array"),
+      Equals<std::string>({"hello", "joe"}));
+
+  auto global_map = js.getGlobal<std::unordered_map<std::string, int>>("a_map");
+  REQUIRE(2 == global_map.size());
+  REQUIRE(1 == global_map.at("hello"));
+  REQUIRE(2 == global_map.at("bloe"));
+
+  auto script_3 = js.load(kScript3);
+  REQUIRE(3.2 == js.call<double>(script_3, "get_a_number"));
+  REQUIRE(true == js.call<bool>(script_3, "get_a_bool"));
+  REQUIRE(7 == js.call<int>(script_3, "get_an_int"));
+  REQUIRE("foobar" == js.call<std::string>(script_3, "get_a_string"));
+  REQUIRE_THAT(
+      js.call<std::vector<std::string>>(script_3, "get_an_array"),
+      Equals<std::string>({"hello", "joe"}));
+  global_map.clear();
+  global_map =
+      js.call<std::unordered_map<std::string, int>>(script_3, "get_a_map");
+  REQUIRE(2 == global_map.size());
+  REQUIRE(1 == global_map.at("hello"));
+  REQUIRE(2 == global_map.at("bloe"));
+}
+
+}  // namespace tequila
