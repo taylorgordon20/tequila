28a374a646f81b9b5f85a6e47788ad2b0f0423e8
Taylor Gordon <taylorgordon20@gmail.com>
2018-09-27 00:03:08 -0400

    better physics

--- scripts/world_input.lua
+++ scripts/world_input.lua
@@ -2,7 +2,7 @@ local module = {
   orientation_toggle = true,
   orientation_angles = {0, 0},
   physics_mode = false,
-  physics_jump = 0,
+  camera_velocity = {0, 0, 0},
 }
 
 local KEYS = {
@@ -14,65 +14,33 @@ local KEYS = {
 }
 
 local PHYSICS = {
-  gravity = -0.5,
-  terminal_speed = 50.0,
-  jump_speed = 15.0,
+  resistance_force = {-0.5, -0.001, -0.5},
+  gravity_force = -0.4,
+  jump_force = 12.0,
+  walk_force = 10.0,
+  run_force = 50.0,
 }
 
 local PI = 2 * math.asin(1)
 
+local switch = function(condition, true_result, false_result)
+  if condition then
+    return true_result
+  else 
+    return false_result
+  end
+end
+
 local clamp = function(val, min, max)
-  return val < min and min or val > max and max or val
+  return switch(val < min, min, switch(val > max, max, val))
 end
 
 local clamp_abs = function(val, size)
-  return clamp(val, -size, size)
+  return clamp(val, -math.abs(size), math.abs(size))
 end
 
-local handle_key_movement = function(dt, move)
-  local speed = is_key_pressed(KEYS.left_shift) and 50.0 or 10.0
-  local view_x, view_y, view_z = table.unpack(get_camera_view())
-  if is_key_pressed(string.byte('W')) then
-    move[1] = move[1] + speed * dt * view_x
-    move[2] = move[2] + speed * dt * view_y
-    move[3] = move[3] + speed * dt * view_z
-  end
-  if is_key_pressed(string.byte('S')) then
-    move[1] = move[1] - speed * dt * view_x
-    move[2] = move[2] - speed * dt * view_y
-    move[3] = move[3] - speed * dt * view_z
-  end
-  if is_key_pressed(string.byte('A')) then
-    move[1] = move[1] + speed * dt * view_z
-    move[3] = move[3] - speed * dt * view_x
-  end
-  if is_key_pressed(string.byte('D')) then
-    move[1] = move[1] - speed * dt * view_z
-    move[3] = move[3] + speed * dt * view_x
-  end
-  if is_key_pressed(KEYS.pg_up) then
-    move[2] = move[1] + speed * dt
-  end
-  if is_key_pressed(KEYS.pg_dn) then
-    move[2] = move[1] - speed * dt
-  end
-end
-
-local clamp_movement = function(move)
-  local x, y, z = table.unpack(get_camera_pos())
-  local fx = x - math.floor(x)
-  local fy = y - math.floor(y)
-  local fz = z - math.floor(z)
-  local lv = get_voxel(x - 1, y, z) ~= 0 and 1 or 0
-  local rv = get_voxel(x + 1, y, z) ~= 0 and 1 or 0
-  local dv = get_voxel(x, y - 1, z) ~= 0 and 1 or 0
-  local uv = get_voxel(x, y + 1, z) ~= 0 and 1 or 0
-  local bv = get_voxel(x, y, z - 1) ~= 0 and 1 or 0
-  local fv = get_voxel(x, y, z + 1) ~= 0 and 1 or 0
-  local px, py, pz = table.unpack({0.4, 0.8, 0.4})
-  move[1] = clamp(move[1], lv * (1 - fx + px) - 1, 1 - rv * (fx + px))
-  move[2] = clamp(move[2], dv * (1 - fy + py) - 1, 1 - uv * (fy + py))
-  move[3] = clamp(move[3], bv * (1 - fz + pz) - 1, 1 - fv * (fz + pz))
+local truncate = function(val, threshold)
+  return switch(math.abs(val) <= threshold, 0, val)
 end
 
 local for_camera_ray_voxels = function(voxel_fn)
@@ -91,6 +59,124 @@ local for_camera_ray_voxels = function(voxel_fn)
   end
 end
 
+local clamp_movement = function(from, move, dim)
+  local pad = 0.001
+  local p = from[dim]
+  local m = move[dim]
+  local s = {0, 0, 0}
+  if dim == 1 then
+    s = {switch(m < 0, -1, 1), 0, 0}
+  elseif dim == 2 then
+    s = {move[1], switch(m < 0, -1, 1), 0}
+  else
+    s = {move[1], move[2], switch(m < 0, -1, 1)}
+  end
+  if get_voxel(from[1] + s[1], from[2] + s[2], from[3] + s[3]) ~= 0 then
+    move[dim] = clamp(m, math.floor(p) - p + pad, math.floor(p + 1) - p - pad)
+  end
+end
+
+function module:get_flying_movement(dt)
+  local move = {0, 0, 0}
+  local force = switch(
+      is_key_pressed(KEYS.left_shift),
+      PHYSICS.run_force,
+      PHYSICS.walk_force
+  )
+  local view_x, view_y, view_z = table.unpack(get_camera_view())
+  if is_key_pressed(string.byte('W')) then
+    move[1] = move[1] + force * dt * view_x
+    move[2] = move[2] + force * dt * view_y
+    move[3] = move[3] + force * dt * view_z
+  end
+  if is_key_pressed(string.byte('S')) then
+    move[1] = move[1] - force * dt * view_x
+    move[2] = move[2] - force * dt * view_y
+    move[3] = move[3] - force * dt * view_z
+  end
+  if is_key_pressed(string.byte('A')) then
+    move[1] = move[1] + force * dt * view_z
+    move[3] = move[3] - force * dt * view_x
+  end
+  if is_key_pressed(string.byte('D')) then
+    move[1] = move[1] - force * dt * view_z
+    move[3] = move[3] + force * dt * view_x
+  end
+  if is_key_pressed(KEYS.pg_up) then
+    move[2] = move[1] + force * dt
+  end
+  if is_key_pressed(KEYS.pg_dn) then
+    move[2] = move[1] - force * dt
+  end
+  return move
+end
+
+function module:get_physics_movement(dt)
+  -- Start with the inertial velocity.
+  local move = {
+    dt * self.camera_velocity[1],
+    dt * self.camera_velocity[2],
+    dt * self.camera_velocity[3],
+  }
+
+  -- Apply a force of keyboard-induced self-propulsion.
+  local orient_x = math.sin(self.orientation_angles[1])
+  local orient_z = math.cos(self.orientation_angles[1])
+  local propulsion = switch(
+      is_key_pressed(KEYS.left_shift),
+      PHYSICS.run_force,
+      PHYSICS.walk_force
+  )
+  if is_key_pressed(string.byte('W')) then
+    move[1] = move[1] + dt * propulsion * orient_x
+    move[3] = move[3] + dt * propulsion * orient_z
+  end
+  if is_key_pressed(string.byte('S')) then
+    move[1] = move[1] - dt * propulsion * orient_x
+    move[3] = move[3] - dt * propulsion * orient_z
+  end
+  if is_key_pressed(string.byte('A')) then
+    move[1] = move[1] + dt * propulsion * orient_z
+    move[3] = move[3] - dt * propulsion * orient_x
+  end
+  if is_key_pressed(string.byte('D')) then
+    move[1] = move[1] - dt * propulsion * orient_z
+    move[3] = move[3] + dt * propulsion * orient_x
+  end
+
+  -- Apply a gravity force.
+  move[2] = move[2] + dt * PHYSICS.gravity_force
+
+  -- Add the resistance force.
+  move[1] = (1 + PHYSICS.resistance_force[1]) * move[1]
+  move[2] = (1 + PHYSICS.resistance_force[2]) * move[2]
+  move[3] = (1 + PHYSICS.resistance_force[3]) * move[3]
+
+  -- Truncate the movement vector to zero below some threshold.
+  move[1] = truncate(move[1], 0.00001)
+  move[2] = truncate(move[2], 0.00001)
+  move[3] = truncate(move[3], 0.00001)
+
+  -- Clamp the movement vector to avoid collisions.
+  local cx, cy, cz = table.unpack(get_camera_pos())
+  for dim = 1, 3 do
+    for _, dx in ipairs({-0.45, 0.45}) do
+      for _, dy in ipairs({-0.75, 0, 0.75}) do
+        for _, dz in ipairs({-0.45, 0.45}) do
+          clamp_movement({cx + dx, cy + dy, cz + dz}, move, dim)
+        end
+      end
+    end
+  end
+
+  -- Record the inertial velocity.
+  self.camera_velocity[1] = move[1] / dt
+  self.camera_velocity[2] = move[2] / dt
+  self.camera_velocity[3] = move[3] / dt
+ 
+  return move
+end
+
 function module:on_init()
   print("Initialized world_input.lua")
   set_cursor_visible(false)
@@ -102,12 +188,12 @@ function module:on_key(key, scancode, action, mods)
     set_cursor_visible(not self.orientation_toggle)
   elseif key == string.byte('G') and action == 1 then
     self.physics_mode = not self.physics_mode
-    self.physics_jump = 0
+    self.camera_velocity = {0, 0, 0}
   elseif key == KEYS.home and action == 1 then
     self.orientation_angles = {0, 0}
     set_camera_view(0, 0, 1)
   elseif key == KEYS.space and action == 1 then
-    self.physics_jump = PHYSICS.jump_speed
+    self.camera_velocity = {0, PHYSICS.jump_force, 0}
   end
 end
 
@@ -139,25 +225,16 @@ end
 
 function module:on_update(dt)
   local camera_move = {0, 0, 0}
-
-  -- Update the camera move vector based on key input.
-  handle_key_movement(dt, camera_move)
-
-  -- Update the camera move vector based on phyics.
   if self.physics_mode then
-    -- Apply the current jump velocity to the movement vector.
-    self.physics_jump = self.physics_jump + PHYSICS.gravity
-    self.physics_jump = clamp_abs(self.physics_jump, PHYSICS.terminal_speed)
-    camera_move[2] = camera_move[2] + dt * self.physics_jump
-
-    -- Clamp the movement vector to avoid collisions.
-    clamp_movement(camera_move)
+    camera_move = self:get_physics_movement(dt)
+  else
+    camera_move = self:get_flying_movement(dt)
   end
 
   if camera_move[1] ~= 0 or camera_move[2] ~= 0 or camera_move[2] ~= 0 then
     local cx, cy, cz = table.unpack(get_camera_pos())
-    local vx, vy, vz = table.unpack(camera_move)
-    set_camera_pos(cx + vx, cy + vy, cz + vz)
+    local mx, my, mz = table.unpack(camera_move)
+    set_camera_pos(cx + mx, cy + my, cz + mz)
   end
 
   -- Update the camera orientation as the cursor moves.
