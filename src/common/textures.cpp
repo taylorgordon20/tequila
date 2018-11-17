afb1f1fe8c656b7ec14dd91c6f1252416b0e72fb
Taylor Gordon <taylorgordon20@gmail.com>
2018-11-17 10:03:06 -0500

    more trees and clamp texture output

--- src/common/textures.cpp
+++ src/common/textures.cpp
@@ -59,6 +59,8 @@ TextureOutput::TextureOutput(int width, int height)
 
   // Set the textures pixel data.
   glBindTexture(GL_TEXTURE_2D, texture_);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
   glTexImage2D(
