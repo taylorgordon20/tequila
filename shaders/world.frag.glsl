a5b877173dff403ba397ffc950486db3aa89980f
Taylor Gordon <taylorgordon20@gmail.com>
2018-11-23 21:20:45 -0500

    tweak boken

--- shaders/world.frag.glsl
+++ shaders/world.frag.glsl
@@ -34,7 +34,7 @@ float invertDepth(float depth) {
 
 float depthMask(float scene_depth) {
   float speed = 0.02;
-  float pivot = 120;
+  float pivot = 150;
   float depth = invertDepth(scene_depth);
   return 1.0 / (1 + exp(speed * (pivot - depth)));
 }
